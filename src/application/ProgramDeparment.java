a4cec2a6d42cece84712d12c12d6af1092cb1eb3
jamor <jamor.devbr@gmail.com>
2023-10-16 19:35:39 -0300

    insert department

--- src/application/ProgramDeparment.java
+++ src/application/ProgramDeparment.java
@@ -16,11 +16,16 @@ public class ProgramDeparment {
 		Department department = departmentDao.findById(1);
 		System.out.println(department);
 		
-		System.out.println("\n=== TEST 3: department findAll ===");	
+		System.out.println("\n=== TEST 2: department findAll ===");	
 		List<Department> list = departmentDao.findAll();
 		for(Department obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: department insert ===");	
+		Department newDepartment = new Department(null, "Music");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
 
 	}
 
